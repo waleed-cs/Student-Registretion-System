a91b1576444b05f9275ab0b0ad6b01cea7438353
waleed-cs <waleedalharthi1999@gmail.com>
2021-08-19 11:13:55 +0300

    first commit

--- js/login.js
+++ js/login.js
@@ -4,7 +4,9 @@ const password = document.querySelector("#password");
 eyeIcon.addEventListener("click", () => {
   if (password.type == "password") {
     password.type = "text";
+    eyeIcon.classList.add("fa-eye-slash");
   } else {
     password.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
   }
 });
